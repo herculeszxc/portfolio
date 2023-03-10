f89a47109ca9b80ca0244fa773082f31ad0117a2
Herczxc <62851950+herculeszxc@users.noreply.github.com>
2023-03-10 12:09:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -74,16 +74,5 @@ class StickyNavigation {
 
 new StickyNavigation();
 
-//visitor
-const countEl = document.getElementById('count');
 
-updateVisitCount();
-
-function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/herc/port/?amount=1')
-	.then(res => res.json())
-	.then(res => {
-		countEl.innerHTML = res.value;
-	})
-}
 
